1bab9d46e9d498dc7d851e13f9031232f3f429d4
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-18 15:04:24 +0800

    feat[forms-register]:  update min length for password, create error feedback on invalid field input

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
