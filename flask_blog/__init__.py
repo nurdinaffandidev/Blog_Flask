aec8192b4de03dc64c1c175f49e9d66071a3a11b
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-19 15:11:52 +0800

    feat[authentication]: add Bcrypt to app for hashing of password

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -12,5 +13,7 @@ app.config['SECRET_KEY'] = '7f27bc7be8123abfc4ad055db736e8aa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # create database instance
 db = SQLAlchemy(app)
+# configure Bcrypt
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
