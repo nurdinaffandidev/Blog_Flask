58d94e912eb788bcc69182a8fb4a709e77858aba
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-24 13:28:00 +0800

    feat[posts]: create new post route

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -3,7 +3,7 @@ import os
 
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
@@ -125,3 +125,13 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash(f'Your post has been created!', 'success')  # flash: to send a one-time alert
+        return redirect(url_for('index'))
+    return render_template('create_post.html', title='New Post', form=form)
