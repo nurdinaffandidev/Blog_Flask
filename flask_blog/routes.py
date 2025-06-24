6ce023bbd2367575ced7db5c843b1ffb78e87319
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-24 14:52:20 +0800

    feat[posts]: create route to update post via id, update create_post template legend with dynamic value

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,7 @@
 import secrets
 import os
 
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_blog import app, db, bcrypt
 from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_blog.models import User, Post
@@ -124,10 +124,34 @@ def new_post():
         db.session.commit()
         flash(f'Your post has been created!', 'success')  # flash: to send a one-time alert
         return redirect(url_for('index'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    # get post if exist else throw 404 error
+    post = Post.query.get_or_404(post_id)
+    # check authorized user for post else throw 403 error
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        # update post and commit to db
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id)) # redirect required due to post-get-redirect pattern
+    elif request.method == 'GET':
+        # populate form with existing post title and content
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
