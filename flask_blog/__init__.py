2653782db5b3d97576f2e290fc26906caa21b7da
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-25 17:09:47 +0800

    feat[pw-reset]: add configuration for email server

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -21,5 +24,12 @@ login_manager = LoginManager(app)
 # configure login view of login_manager
 login_manager.login_view = 'login' # 'login' => function name of route
 login_manager.login_message_category = 'info'
+#configure email server
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flask_blog import routes
