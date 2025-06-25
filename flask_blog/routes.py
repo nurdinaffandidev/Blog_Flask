c9b8a8671796c911a89031c06e7313239a6dab3e
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-25 10:36:07 +0800

    feat[pagination]: update posts query to paginate, set default page

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -13,7 +13,11 @@ from PIL import Image
 @app.route("/")
 @app.route("/home")
 def index():
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    # setting default page
+    page = request.args.get('page', 1, type=int)
+    # setting number of post per page
+    # Note: this results in posts becoming a Pagination object
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
