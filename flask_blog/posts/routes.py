788281b8608d0447c23b1b8789b95e1c9830d6ef
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-26 14:22:12 +0800

    feat[blueprints-posts]: refactor routes

---
+++ flask_blog/posts/routes.py
@@ -0,0 +1,60 @@
+from flask import Blueprint
+
+# create instance of Blueprint
+posts = Blueprint('posts', __name__)
+
+
+@posts.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        # creating post and adding post to db
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')  # flash: to send a one-time alert
+        return redirect(url_for('index'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@posts.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    # get post if exist else throw 404 error
+    post = Post.query.get_or_404(post_id)
+    # check authorized user for post else throw 403 error
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        # update post and commit to db
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id)) # redirect required due to post-get-redirect pattern
+    elif request.method == 'GET':
+        # populate form with existing post title and content
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+
+
+@posts.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('index'))
