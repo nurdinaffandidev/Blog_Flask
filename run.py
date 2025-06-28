1e333f91f1011b6b7df791eaca3274ecc8b4397f
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-06-28 16:23:38 +0800

    fix: recreate overridden db from test

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
-from flask_blog import create_app
+from flask_blog import create_app, db
 
 
 app = create_app()
+
+# to run on first app run
+# with app.app_context():
+#     db.create_all()
+#     print("✅ Database tables created!")
 
 # __name__ = main when we run script with python directly in CLI(command-line interface)
 if __name__ == '__main__':
